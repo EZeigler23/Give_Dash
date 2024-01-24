69907235c22d249e4bfe5c4dd517d528c873ce2e
Elancio Zeigler <elancioz12@gmail.com>
2024-01-24 09:11:07 -0500

    Initial Commit

---
+++ Give_Dash/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Give_Dash
+//
+//  Created by Elancio Zeigler on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("GDbg")
+            VStack {
+                Image("GDlogo1")
+                
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Give_Dash/Give_DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Give_DashApp.swift
+//  Give_Dash
+//
+//  Created by Elancio Zeigler on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Give_DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

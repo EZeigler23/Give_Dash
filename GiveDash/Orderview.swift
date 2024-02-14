cbb3b217f3387636609267b249d8586e5409b3e9
Elancio Zeigler <elancioz12@gmail.com>
2024-02-13 21:02:25 -0500

    close to last update

--- GiveDash/Orderview.swift
+++ GiveDash/Orderview.swift
@@ -12,7 +12,7 @@ struct OrderView: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 42.3314, longitude: -83.0458),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-    @State private var isMapClicked = false
+    @State private var selectedLocation: CLLocationCoordinate2D?
     
     private func updateMapLocation() {
         let geocoder = CLGeocoder()
@@ -21,6 +21,7 @@ struct OrderView: View {
                 print("Location error: \(error.localizedDescription)")
             } else if let placemark = placemarks?.first {
                 region.center = placemark.location?.coordinate ?? region.center
+                selectedLocation = placemark.location?.coordinate
             } else {
                 print("No places found.")
             }
@@ -34,6 +35,7 @@ struct OrderView: View {
         "44 million people in the US face hunger, including 1 in 5 children in 2022",
         "1 in 8 Americans are estimated to be food insecure.","Food production and distribution systems contribute to environmental degradation, including deforestation, greenhouse gas emissions, and loss of biodiversity, further exacerbating food insecurity in the long term.", "Food insecurity disproportionately affects vulnerable populations, including children, women, the elderly, and marginalized communities.","Economic instability, poverty, conflict, and climate change are among the primary factors contributing to food insecurity."," Food insecurity can lead to malnutrition, which in turn contributes to various health issues, including stunted growth, weakened immune systems, and higher susceptibility to diseases.","Prior to 2024, around 9% of the world's population was estimated to be undernourished, according to the Food and Agriculture Organization (FAO) of the United Nations"
     ]
+    
     var body: some View {
         
         NavigationView {
@@ -75,24 +77,14 @@ struct OrderView: View {
                             }
                             .padding()
                             
-                            if !isMapClicked {
-                                Map(coordinateRegion: $region)
-                                    .frame(height: 200)
-                                    .cornerRadius(16)
-//                                    .onTapGesture {
-//                                        isMapClicked.toggle()
-//                                    }
+                            
+                            Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.none), annotationItems: [selectedLocation].compactMap { $0 }) { location in
+                                MapPin(coordinate: location)
                             }
-                            else {
-                                Map(coordinateRegion: $region)
-                                    .frame(maxHeight: .infinity)
-                                    .cornerRadius(16)
-                                    .onTapGesture {
-                                        isMapClicked.toggle()
-                                    }
-                            }
+                            .frame(height: 200)
+                            .cornerRadius(16)
+                            
                             HStack{
-                                
                                 TextField("Enter location (city or address)", text: $userLocationInput, onCommit: {
                                     updateMapLocation()
                                 })
@@ -101,14 +93,14 @@ struct OrderView: View {
                                 .multilineTextAlignment(.center)
                                 .autocapitalization(.words)
                                 .padding(.horizontal)
-                                .keyboardType(.webSearch) // Allow "Return" key on the keyboard
                                 
                                 Button("Update") {
                                     updateMapLocation()
-                                }  .padding()
-                                    .foregroundColor(.white)
-                                    .background(Color.gDbcolor)
-                                    .cornerRadius(10)
+                                }
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.gDbcolor)
+                                .cornerRadius(10)
                             }
                             
                             // Additional content for the OrderView can be added below the map
@@ -119,7 +111,7 @@ struct OrderView: View {
                             Spacer()
                                 .padding()
                             
-                            NavigationLink(destination: GroceryListView()) {
+                            NavigationLink(destination: Text("Grocery List View")) {
                                 Text("Place Order")
                                     .bold()
                                     .padding()
@@ -127,80 +119,23 @@ struct OrderView: View {
                                     .background(Color.gDbcolor)
                                     .cornerRadius(10)
                             }
-                                .padding()
+                            .padding()
                             Spacer()
                         }
                     }
-                        .padding()
-                        .navigationBarTitle("Order", displayMode: .inline)
-                        .bold()
-                    )}
-                    
-//                    if !isMapClicked {
-//                        Map(coordinateRegion: $region)
-//                            .frame(height: 200)
-//                            .cornerRadius(16)
-//                            .onTapGesture {
-//                                isMapClicked.toggle()
-//                            }
-//                    }
-//                    else {
-//                        Map(coordinateRegion: $region)
-//                            .frame(maxHeight: .infinity)
-//                            .cornerRadius(16)
-//                            .onTapGesture {
-//                                isMapClicked.toggle()
-//                            }
-//                    }
-//                    HStack{
-//                        
-//                        TextField("Enter location (city or address)", text: $userLocationInput, onCommit: {
-//                            updateMapLocation()
-//                        })
-//                        .textFieldStyle(RoundedBorderTextFieldStyle())
-//                        .padding()
-//                        .multilineTextAlignment(.center)
-//                        .autocapitalization(.words)
-//                        .padding(.horizontal)
-//                        .keyboardType(.webSearch) // Allow "Return" key on the keyboard
-//                        
-//                        Button("Enter") {
-//                            updateMapLocation()
-//                        }  .padding()
-//                            .foregroundColor(.white)
-//                            .background(Color.gDbcolor)
-//                            .cornerRadius(10)
-//                    }
-//                    
-//                    // Additional content for the OrderView can be added below the map
-//                    Text("Place your order!")
-//                        .font(.title)
-//                        .foregroundColor(.gDbcolor)
-//                        .padding()
-//                    
-//                    
-//                    NavigationLink(destination: GroceryListView()) {
-//                        Text("Place Order")
-//                            .padding()
-//                            .foregroundColor(.gDbcolor)
-//                            .background(Color.gdc2)
-//                            .cornerRadius(10)
-//                    }
-//                        .padding()
+                    .padding()
+                    .navigationBarTitle("Order", displayMode: .inline)
+                    .bold()
+                )}
     }
-//
-//                    
-                    }
-
-                    
-                    
-                    
-                    
-                    
-                    struct OrderView_Previews: PreviewProvider {
-                        static var previews: some View {
-                            OrderView()
-                        }
-                    }
-                    
-                    
+}
+extension CLLocationCoordinate2D: Identifiable {
+    public var id: String {
+        "\(latitude)-\(longitude)"
+    }
+}
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
